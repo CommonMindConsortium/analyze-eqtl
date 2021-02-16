5a0015f32ca14b9691b6294659c06260b26b246b
kelshmo <kelsey.montgomery@sagebase.org>
2021-02-16 21:22:12 +0000

    export synid unquoted

---
+++ parse_synid.R
@@ -0,0 +1,11 @@
+######### Get input synapseId based on chromosome annotations
+load_packages("argparse")
+parser <- ArgumentParser(
+  description = "Get input Synapse id."
+)
+parser$add_argument("chromosome", type="character",
+                    help="chromosome value from 1-22")
+args <- parser$parse_args()
+
+df <- read.table("SYNAPSE_TABLE_QUERY_72602897.csv", sep = ",", header = TRUE)
+cat(as.character(df$id[df$chromosome == args$chromosome]))
