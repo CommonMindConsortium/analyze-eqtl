ef74d85afb4c3b95bdb1b8740c5ca34778ac9f8f
kelshmo <kelsey.montgomery@sagebase.org>
2021-03-23 16:10:15 +0000

    dump hbcc scripts

---
+++ iterate_peer.R
@@ -0,0 +1,32 @@
+######### This script will take the base covariate matrix and append i number of
+######### PEER factors
+library(readr)
+library(dplyr)
+library(glue)
+library(argparse)
+
+# create argument options
+parser <- ArgumentParser(
+  description = "Append x PEER factors to covariate matrix"
+)
+parser$add_argument("covariates", type="character",
+                    help="path to txt with covariate matrix")
+parser$add_argument("peer", type="character",
+                    help="path to PEER factor matrix")
+parser$add_argument("iteration", type = "double",
+                    help = "number of PEER factor to include")
+args <- parser$parse_args()
+
+covars <- read_tsv(args$covariates)
+peer <- read_tsv(args$peer)
+
+dat <- bind_rows(covars, peer[0:args$iteration,])
+
+write.table(
+  dat,
+  glue("tmp_metadata_{args$iteration}.txt"),
+  sep = "\t", 
+  row.names = FALSE, 
+  quote = FALSE
+  )
+
--- rename_vcfs.sh
+++ rename_vcfs.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/bash
 export PATH="$PATH:/usr/bin/bcftools-1.10"
-names="HBCC_h1m_genotyping_samples_rename.txt"
-parent="syn22220656"
-query="SELECT * FROM syn23667032 where cohort = 'HBCC' and genotypingArray = 'H1M' and fileFormat = 'vcf'"
-synapse --configPath "/tmp/.synapseConfig" get -q ${query}
+names="HBCC_h650K_genotyping_samples_rename.txt"
+parent="syn22220655"
 for ((CHROM=1;CHROM<=22;CHROM++));
 do
    echo "# Running chromosome: $CHROM"
